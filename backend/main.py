bb4fedbed98db1940aae4904893373ec534271f4
lucas <s1183103@student.windesheim.nl>
2025-04-17 16:17:39 +0200

    first commit

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+
+app = FastAPI()
+
+# Allow Vue.js frontend to communicate with FastAPI backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:443"],  # Vue dev server
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+@app.get("/backend")
+def read_root():
+    return {"message": "Hello from FastAPI!"}
+
+
+@app.get("/backend/test")
+def read_root():
+    return RedirectResponse(url="http://localhost:5173")
---
+++ frontend/src/pages/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const message = ref("");
+
+    onMounted(async () => {
+      const response = await fetch("http://127.0.0.1:8000/");
+      const data = await response.json();
+      message.value = data.message;
+    });
+
+    return { message };
+  }
+};
+</script>
